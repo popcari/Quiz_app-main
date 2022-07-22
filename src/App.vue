ede2d6185bf7e38e355bfbf1e2679ceee9656138
duy.ntHachix <duy.nt1009@gmail.com>
2022-07-22 14:56:49 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,143 @@
+<template>
+  <div class="ctr">
+    <transition name="fade" mode="out-in">
+      <question v-if="questionsAnswered < questions.length"  
+      :questions="questions" 
+      :questionsAnswered="questionsAnswered" 
+      @question-answered="questionAnswered"
+      />
+      <result v-else 
+      :results="results"
+      :totalCorrect="totalCorrect"
+      />
+    </transition>
+
+    <button 
+    @click.prevent="reset"
+    type="button" 
+    class="reset-btn"
+    v-if="questionAnswered === questions.length"
+    >Reset</button>
+  </div>
+</template>
+
+<script>
+import Question from "./components/QuestionCom.vue";
+import Result from "./components/ResultCom.vue";
+
+export default {
+  name: "App",
+  components: {
+    Question,
+    Result,
+  },
+  data() {
+    return {
+      questionsAnswered: 0,
+      totalCorrect: 0,
+
+      questions: [
+        {
+          q: "What is 2 + 2?",
+          answers: [
+            {
+              text: "4",
+              is_correct: true,
+            },
+            {
+              text: "3",
+              is_correct: false,
+            },
+            {
+              text: "Fish",
+              is_correct: false,
+            },
+            {
+              text: "5",
+              is_correct: false,
+            },
+          ],
+        },
+        {
+          q: 'How many letters are in the word "Banana"?',
+          answers: [
+            {
+              text: "5",
+              is_correct: false,
+            },
+            {
+              text: "7",
+              is_correct: false,
+            },
+            {
+              text: "6",
+              is_correct: true,
+            },
+            {
+              text: "12",
+              is_correct: false,
+            },
+          ],
+        },
+        {
+          q: "Find the missing letter: C_ke",
+          answers: [
+            {
+              text: "e",
+              is_correct: false,
+            },
+            {
+              text: "a",
+              is_correct: true,
+            },
+            {
+              text: "i",
+              is_correct: false,
+            },
+          ],
+        },
+      ],
+      results: [
+        {
+          min: 0,
+          max: 2,
+          title: "Try again!",
+          desc: "Do a little more studying and you may succeed!",
+        },
+        {
+          min: 3,
+          max: 3,
+          title: "Wow, you're a genius!",
+          desc: "Studying has definitely paid off for you!",
+        },
+      ],
+    };
+  },
+
+  methods:{
+   questionAnswered(is_correct){
+    if(is_correct){
+      this.totalCorrect++;
+    }
+
+    this.questionsAnswered++;
+   } ,
+
+   reset(){
+    this.questionAnswered = 0
+    this.totalCorrect = 0
+   }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ResultCom.vue
@@ -0,0 +1,44 @@
+<template>
+   <div class="result">
+        <div class="title">{{results[resultIndex].title}}</div>
+        <div class="desc">
+            {{results[resultIndex].desc}}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+name: 'ResultCom',
+props:{results:{
+    type: Array
+}
+,totalCorrect:{
+    type: Array
+}},
+data(){
+
+},
+methods:{
+
+},
+computed:{
+    resultIndex(){
+        let index = 0;
+// e: element, i: index
+    this.results.forEach((e, i) => {
+    if(e.min <= this.totalCorrect && e.max >= this.totalCorrect ){
+        index = i;
+
+    }
+});
+
+        return index;
+    }
+}
+}
+</script>
+
+<style>
+
+</style>
